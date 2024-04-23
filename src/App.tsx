945c3347b0ba79bc2fa9264f33c8b9a286ce1904
sushanshakya77 <sushanshakya77@gmail.com>
2024-04-23 21:45:49 +0545

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <a href="/docs">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Caesar Cipher
+                </NavigationMenuLink>
+              </a>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <a href="/docs">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Monoalphabetic Cipher
+                </NavigationMenuLink>
+              </a>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
